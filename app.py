1b008abcccdd05b5c50da6fc1f383a32f3876671
Kabir Borle <kabirborle@gmail.com>
2023-09-24 05:23:47 -0500

    fixed backend
    fixed + added bart functionality

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import requests
 from transformers import pipeline
 from flask_sqlalchemy import SQLAlchemy
-import os
-from bs4 import BeautifulSoup
+from newsdataapi import NewsDataApiClient
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsdb.sqlite3'
@@ -14,9 +13,8 @@ db = SQLAlchemy(app)
 summarizer = pipeline("summarization")
 
 # Constants
-API_ENDPOINT = "https://newsapi.org/v2/everything"
-API_KEY = "9f5e7057d622425db2b24106840aeb5a"  # Replace with your actual NewsAPI key
-HARDCODED_KEYWORDS = ["technology", "science", "health"]  # You can change or add more keywords
+api = NewsDataApiClient(apikey="pub_2999839b534cd154687eeb2e90f72d19aec9d") # Replace with your actual NewsAPI key
+HARDCODED_KEYWORDS = ["Sustainability", "Legal", "Energy"]  # You can change or add more keywords
 
 @app.route('/get-news-summaries', methods=['GET'])
 def get_news_summaries():
@@ -30,71 +28,58 @@ def get_stored_articles():
     articles = Article.query.all()
     return jsonify([{
         "title": article.title,
-        "summary": article.summary,
-        "url": article.url,
-        "image_url": article.image_url,
-        "keyword": article.keyword
+        "summary": article.summary,  # Use 'description' instead of 'summary'
+        "url": article.url,                # Use 'link' instead of 'url'
+        "image_url": article.image_url,      # This remains the same
+        "creator": article.creator,
+        "description": article.description        # Use 'keywords' which is a list, instead of a single 'keyword'
     } for article in articles])
 
+
 def fetch_articles(keywords):
-    headers = {"Authorization": f"Bearer {API_KEY}"}
     all_articles = []
 
     for keyword in keywords:
-        params = {
-            "q": keyword,
-            "language": "en",
-            "pageSize": 1  # Fetching 5 articles per keyword; adjust as needed
-        }
-
-        response = requests.get(API_ENDPOINT, headers=headers, params=params)
-        response.raise_for_status()
-
-        articles_for_keyword = response.json().get('articles', [])
+        response = api.news_api(q=keyword)["results"]
         
-        # Add the keyword to each article's data
-        for article in articles_for_keyword:
-            article['keyword'] = keyword
-        
+        # Assuming 'response' is a list of articles
+        articles_for_keyword = response[:3]  # Limit to 2 articles
+
+
         all_articles.extend(articles_for_keyword)
 
     return all_articles
 
-def get_full_article_content(url): #helper function for summarize_articles. scrapes content from article. 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-        paragraphs = soup.find_all('p')
-        article_content = ' '.join(para.text for para in paragraphs)
-        return article_content
-    except Exception as e:
-        print(f"Error fetching full content for {url}: {e}")
-        return None
     
 def summarize_articles(articles):
     summarized_articles = []
 
     for article in articles:
         title = article.get("title")
-        content = article.get("content", "")
-        url = article.get("url")
-        image_url = article.get("urlToImage")  # Extract the image URL
-        keyword = article.get("keyword")
+        description = article.get("description")
+        content = article.get("content")
+        url = article.get("link")
+        image_url = article.get("image_url")  # Extract the image URL
+        if(article.get("creator"))!=None:
+            creator = str(article.get("creator"))
+        else:
+            creator = "None"
 
-        content = get_full_article_content(url)
 
         if not content:
             continue
+        
+        content = content[:500]
 
-        summary = summarizer(content, max_length=300, min_length=100, do_sample=True)[0]['summary_text']
+        summary = summarizer(content, max_length=800, min_length=50, do_sample=False)[0]['summary_text']
+        if len(description)>220:
+            description = summarizer(content, max_length=220, min_length=40, do_sample=False)[0]['summary_text']
 
         # Check if the article already exists in the database
         existing_article = Article.query.filter_by(url=url).first()
 
         if not existing_article:
-            article_entry = Article(title=title, summary=summary, url=url, image_url=image_url, keyword=keyword)
+            article_entry = Article(title=title, summary=summary, url=url, image_url=image_url, creator=creator, description = description)
             db.session.add(article_entry)
             db.session.commit()
 
@@ -102,11 +87,13 @@ def summarize_articles(articles):
             "title": title,
             "summary": summary,
             "url": url,
-            "image_url": image_url,  # Add the image URL to the summarized article data
-            "keyword" : keyword 
+            "image_url": image_url,
+            "creator" : creator,
+            "description" : description
         })
 
     return summarized_articles
+
 
 #Define Database Model
 
@@ -116,7 +103,8 @@ class Article(db.Model):
     summary = db.Column(db.Text, nullable=False)
     url = db.Column(db.String(500), nullable=False)
     image_url = db.Column(db.String(500), nullable=True)
-    keyword = db.Column(db.String(100), nullable=False)
+    creator = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable = False)
 
 with app.app_context():
         db.create_all()
