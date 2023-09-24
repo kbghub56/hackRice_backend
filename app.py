5ac8102d6e3bf4674797e55e60fe9ce312102cb6
Kabir Borle <kabirborle@gmail.com>
2023-09-23 22:43:24 -0500

    Added SQLite
    Added database functionality. Call curl http://localhost:5000/get-news-summaries , to initially populate database, then curl http://localhost:5000/get-stored-articles  to view the database

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
 from transformers import pipeline
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsdb.sqlite3'
+db = SQLAlchemy(app)
 
 # Initialize the summarizer
 summarizer = pipeline("summarization")
@@ -21,6 +23,17 @@ def get_news_summaries():
 
     return jsonify(summarized_articles)
 
+@app.route('/get-stored-articles', methods=['GET'])
+def get_stored_articles():
+    articles = Article.query.all()
+    return jsonify([{
+        "title": article.title,
+        "summary": article.summary,
+        "url": article.url,
+        "image_url": article.image_url,
+        "keyword": article.keyword
+    } for article in articles])
+
 def fetch_articles(keywords):
     headers = {"Authorization": f"Bearer {API_KEY}"}
     all_articles = []
@@ -35,9 +48,16 @@ def fetch_articles(keywords):
         response = requests.get(API_ENDPOINT, headers=headers, params=params)
         response.raise_for_status()
 
-        all_articles.extend(response.json().get('articles', []))
+        articles_for_keyword = response.json().get('articles', [])
+        
+        # Add the keyword to each article's data
+        for article in articles_for_keyword:
+            article['keyword'] = keyword
+        
+        all_articles.extend(articles_for_keyword)
 
     return all_articles
+
 
 def summarize_articles(articles):
     summarized_articles = []
@@ -47,20 +67,46 @@ def summarize_articles(articles):
         content = article.get("content", "")
         url = article.get("url")
         image_url = article.get("urlToImage")  # Extract the image URL
+        keyword = article.get("keyword")
 
         if not content:
             continue
 
         summary = summarizer(content, max_length=100, min_length=30, do_sample=False)[0]['summary_text']
 
+        # Check if the article already exists in the database
+        existing_article = Article.query.filter_by(url=url).first()
+
+        if not existing_article:
+            article_entry = Article(title=title, summary=summary, url=url, image_url=image_url, keyword=keyword)
+            db.session.add(article_entry)
+            db.session.commit()
+
         summarized_articles.append({
             "title": title,
             "summary": summary,
             "url": url,
-            "image_url": image_url  # Add the image URL to the summarized article data
+            "image_url": image_url,  # Add the image URL to the summarized article data
+            "keyword" : keyword 
         })
 
     return summarized_articles
 
+#Define Database Model
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    summary = db.Column(db.Text, nullable=False)
+    url = db.Column(db.String(500), nullable=False)
+    image_url = db.Column(db.String(500), nullable=True)
+    keyword = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+        db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
